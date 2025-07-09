95d0f25e3b0c25f511f2cfaa4a0d8f6077587e10
Asen Yovchev <ayovcheff@gmail.com>
2025-07-09 14:06:29 +0300

    set onChange prop to be optional

--- src/LazyField.tsx
+++ src/LazyField.tsx
@@ -1,6 +1,6 @@
-import { useDebounceCallback } from "@ayovchev/react-debounce-callback-hook";
-import { FieldAttributes, useField } from "formik";
-import React from "react";
+import { useDebounceCallback } from '@ayovchev/react-debounce-callback-hook';
+import { type FieldAttributes, useField } from 'formik';
+import React from 'react';
 
 export const LazyField = <T,>({ Component, ...props }: FieldAttributes<T>) => {
   const [field, meta] = useField(props);
@@ -12,16 +12,23 @@ export const LazyField = <T,>({ Component, ...props }: FieldAttributes<T>) => {
         name: field.name,
         value: value,
       },
-  })
-    props.onChange(value);
+    });
+
+    props.onChange && props.onChange(value);
   }, props.delay ?? 500);
 
   const handleChange = (e: any) => {
     setValue(e.target.value);
     onChange(e.target.value);
-  }
+  };
 
   return (
-      <Component {...meta} {...field} {...props} onChange={handleChange} value={value} />
+    <Component
+      {...meta}
+      {...field}
+      {...props}
+      onChange={handleChange}
+      value={value}
+    />
   );
 };
